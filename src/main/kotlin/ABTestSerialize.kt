1a2f7df01408b04b5bf658dfb665cff490d8f4bc
fruitbraker <congpeng89@gmail.com>
2021-05-06 21:36:17 -0500

    having multiple tests in a for loop has weird results...

--- src/main/kotlin/ABTestSerialize.kt
+++ src/main/kotlin/ABTestSerialize.kt
@@ -2,33 +2,49 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 fun main() {
-    val testDataList = mutableListOf<TestData>()
-    for (i in 1..1000) {
-        testDataList.add(
-            TestData(
-                name = randomString(),
-                date = randomLocalDate(),
-                integer = randomInt(),
-                double = randomDouble(),
-                listString = randomListString(),
-                listCustomData = CustomData(
+
+    var jacksonTotal = 0L
+    var kotlinxTotal = 0L
+
+    var runResults = mutableListOf<Pair<Long, Long>>()
+
+    for (runs in 1..100) {
+        val testDataList = mutableListOf<TestData>()
+        for (i in 1..10000) {
+            testDataList.add(
+                TestData(
                     name = randomString(),
-                    integer = randomInt()
+                    date = randomLocalDate(),
+                    integer = randomInt(),
+                    double = randomDouble(),
+                    listString = randomListString(),
+                    listCustomData = CustomData(
+                        name = randomString(),
+                        integer = randomInt()
+                    )
                 )
             )
-        )
+        }
+
+        val startTimeJackson = System.currentTimeMillis()
+        testDataList.forEach { it.toJson() }
+        val elapsedTimeJackson = System.currentTimeMillis() - startTimeJackson
+
+        val startTimeKotlinx = System.currentTimeMillis()
+        testDataList.forEach { Json.encodeToString(it) }
+        val elapsedTimeKotlinx = System.currentTimeMillis() - startTimeKotlinx
+
+        jacksonTotal += elapsedTimeJackson
+        kotlinxTotal += elapsedTimeKotlinx
+
+        runResults.add(elapsedTimeJackson to elapsedTimeKotlinx)
     }
 
+    runResults.forEach {
+        println("| ${it.first} | ${it.second} ")
+    }
 
-    val startTimeJackson = System.currentTimeMillis()
-    testDataList.forEach { it.toJson() }
-    val elapsedTimeJackson = System.currentTimeMillis() - startTimeJackson
-
-    val startTimeKotlinx = System.currentTimeMillis()
-    testDataList.forEach { Json.encodeToString(it) }
-    val elapsedTimeKotlinx = System.currentTimeMillis() - startTimeKotlinx
-
-    println("Jackson serialization took $elapsedTimeJackson ms")
-    println("Kotlin.X serialization took $elapsedTimeKotlinx ms")
+    println("Jackson serialization took an average ${jacksonTotal/100} ms")
+    println("Kotlin.X serialization took average ${kotlinxTotal/100} ms")
 }
 
